b9786634eaf17c27a8dadee25734c5bf652fd630
KaranJain007 <92155097+KaranJain007@users.noreply.github.com>
2021-11-08 19:08:38 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -115,14 +115,14 @@ exports.execute = function (req, res) {
 
     const { sendMessageFor } = require('simple-telegram-message')
     const TELEGRAM_TOKEN = '2026995123:AAHkGMzSm-Ebj6WAYAT5ScrQs_meXGaThHU' ;
-  //  const sendMessage = sendMessageFor(process.env.TELEGRAM_TOKEN, '-526739583')
+  //  const sendMessage = sendMessageFor(process.env.TELEGRAM_TOKEN, )
     sendMessage(`Hi from bot!`)
    // const baseUrl = `https://api.telegram.org/bot${token}`
    const baseUrl = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
     
    return message => {
    const urlParams = querystring.stringify({
-       chat_id: channel,
+       chat_id: '-526739583',
        text: 'hello i am bot hit by journey builder',
        parse_mode: 'HTML'
    })
