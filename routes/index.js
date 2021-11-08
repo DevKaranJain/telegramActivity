394f84113c82eafb51975feef664286cf0d0e64c
KaranJain007 <92155097+KaranJain007@users.noreply.github.com>
2021-11-08 18:50:32 +0530

    go

--- routes/index.js
+++ routes/index.js
@@ -31,36 +31,4 @@ exports.logout = function( req, res ) {
     req.session.token = '';
 };
 
-const https = require('https')
-const querystring = require('querystring')
-
-module.exports = {
-  sendMessage,
-  sendMessageFor,
-  sendRequest
-}
-
-function sendMessage (token, channel, message) {
-  return sendMessageFor(token, channel)(message)
-}
-
-function sendMessageFor (token, channel) {
-  const baseUrl = `https://api.telegram.org/bot${token}`
-
-  return message => {
-    const urlParams = querystring.stringify({
-      chat_id: channel,
-      text: 'hello i am onii bot and hit from journey builderr',
-      parse_mode: 'HTML'
-    })
-
-    return sendRequest(`${baseUrl}/sendMessage?${urlParams}`)
-  }
-}
-
-function sendRequest (url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, res => res.statusCode === 200 ? resolve(res) : reject(res))
-      .on('error', reject)
-  })
-}
+    
