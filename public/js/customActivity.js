7cb44af3a3cfb327f5b6b9ea741cba07884134c9
KaranJain007 <92155097+KaranJain007@users.noreply.github.com>
2021-11-17 14:45:00 +0530

    go

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -10,8 +10,8 @@ define([
     var lastStepEnabled = false;
     var steps = [ // initialize to the same value as what's set in config.json for consistency
         { "label": "Create SMS Message", "key": "step1" },
-        { "label": "Choose Chat id destination ", "key": "step2" }
-    //    { "label": "Summary ", "key": "step3" }
+        { "label": "Choose Chat id destination ", "key": "step2" },
+        { "label": "Summary ", "key": "step3" }
     ];
     var currentStep = steps[0].key;
 
@@ -21,15 +21,27 @@ define([
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
 
-    connection.on('clickedNext', save);
-  //  connection.on('clickedBack', onClickedBack);
-  //  connection.on('gotoStep', onGotoStep);
+    connection.on('clickedNext', onClickedNext);
+    connection.on('clickedBack', onClickedBack);
+    connection.on('gotoStep', onGotoStep);
+        //============== start costomization =========
+
+        connection.on('requestedInteraction', function(settings){
+            eventDefinitionKey = settings.triggers[0].metaData.eventDefinitionKey;
+            console.log( " eventDefinitionKey----->" + eventDefinitionKey);
+        });
+        connection.on('requestedSchema', function (data) {
+            // save schema
+            console.log('*** Schema ***', JSON.stringify(data['schema']));
+        });
 
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
+        connection.trigger('requestInteraction');
+        connection.trigger('requestSchema');
     }
 
   function initialize(data) {
@@ -52,11 +64,12 @@ define([
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
 
-                if (key === 'accountSid') {
-                    $('#accountSID').val(val);
-                }
+                // if (key === 'accountSid') {
+                //     $('#accountSID').val(val);
+                // }
                 if (key === 'authToken') {
                     $('#authToken').val(val);
+                    console.log('in the authtoken if --------------------------');
                    
                 }
 
@@ -65,6 +78,7 @@ define([
                 // }
 
                 if (key === 'body') {
+                    console.log('in the message if --------------------------');
                     $('#messageBody').val(val);
                    
                 }                                                               
@@ -74,7 +88,7 @@ define([
         console.log('======----------------------------hello');
         connection.trigger('updateButton', {
             button: 'next',
-            text: 'done',
+            text: 'next',
             visible: true
         });
         console.log('======----------------------------hello');
@@ -92,25 +106,81 @@ define([
         console.log("Get End Points function: "+JSON.stringify(endpoints));
     }
     
-       /*     function onClickedNext(){
-        if((currentStep.key)=='step1')
-        {
-            console.log('in the step 1 if ');
-            connection.trigger('nextStep');
+        function onClickedNext (){
+            var errorSlds = '<div class="slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error" role="alert"><span class="slds-assistive-text">error</span><span class="slds-icon_container slds-icon-utility-error slds-m-right_x-small" title="Description of icon when needed"><svg class="slds-icon slds-icon_x-small" aria-hidden="true"><use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#error"></use></svg></span><h2>Please fill Account SID and Auth Token </h2> <div class="slds-notify__close"><button class="slds-button slds-button_icon slds-button_icon-small slds-button_icon-inverse" title="Close"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#close"></use></svg><span class="slds-assistive-text">Close</span></button></div></div>';
+        //    var checkboxerrorSlds = '<div class="slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error" role="alert"><span class="slds-assistive-text">error</span><span class="slds-icon_container slds-icon-utility-error slds-m-right_x-small" title="Description of icon when needed"><svg class="slds-icon slds-icon_x-small" aria-hidden="true"><use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#error"></use></svg></span><h2>Please select atleast one checkbox</h2> <div class="slds-notify__close"><button class="slds-button slds-button_icon slds-button_icon-small slds-button_icon-inverse" title="Close"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#close"></use></svg><span class="slds-assistive-text">Close</span></button></div></div>';
+    
+            if((currentStep.key) === 'step1')
+            {
+                
+            //  var accountSid = $('#accountSID').val();
+              var authToken = $('#authToken').val();
+                  
+                  if(!authToken )
+                  { 
+                    document.getElementById("error").innerHTML= errorSlds;
+                    connection.trigger('prevStep');
+                  }
+                  else
+                  {
+                    document.getElementById("error").innerHTML= "";
+                    connection.trigger('nextStep');
+                  }
+    
+            } else if ( currentStep.key === 'step2')
+            {
+                
+               // var b =;
+                 var WatsappCheck = $('#messageBody').val(val);  
+                // var SmsCheck =  $("#SMS").is(":checked");
+                // var recipient = $("#recipient").val();
+                // console.log("Recipient ---- " + recipient);
+                // console.log("sms-----" +SmsCheck);
+                // console.log("watsapp------" + WatsappCheck);
 
-        }else if (currentStep.key == 'step2'){
-            connection.trigger('ready');
-            connection.trigger('nextstep');
+                
 
-        } else if (currentStep.key == 'step3' && steps[3].active == false ){
-            save();
-            connection.trigger('nextstep');
-        } else {
-            connection.trigger('nextstep');
-        }
+                if(!WatsappCheck)
+                {
+                    document.getElementById("checkboxcheck").innerHTML= errorSlds;
+                    connection.trigger('ready');
+                }
+                // else if(recipient == "None")
+                // {
+                //     document.getElementById("recipienterror").innerHTML= "Recipient field is empty";
+                //     connection.trigger('ready');
+                // }
+    
+                else 
+                {
+                  //  document.getElementById("recipienterror").innerHTML= "";
+                   // document.getElementById("checkboxcheck").innerHTML= "";
+                    connection.trigger('nextStep');
+                }
+                
+            }
+            
+            else if (currentStep.key === 'step3' && steps[3].active === false) {
+                save();
+            }
+        //    else if ((currentStep.key) === 'step1')
+        //    {
+        //        console.log( "Account SID KE ANDAR HA" ); 
+            //    var accountSid = $('#accountSID').val();
+            //    console.log(accountSid);
+            //    if(accountSid == null)
+            //    {
+            //        alert("AccountSid is empty");
+            //    }
+             //   alert ("Step 1 Next clicked") ;
+        //    }
+            else {
+                console.log("else part me aarha h ");
+                connection.trigger('nextStep');
+            }
     }
 
-  */
+  
     function onClickedBack (){
         connection.trigger('prevStep');
 
@@ -122,7 +192,7 @@ define([
 
     }
 
-  /* function showStep(step, stepIndex) {
+   function showStep(step, stepIndex) {
         console.log('in the showstep function ');
         if (stepIndex && !step) {
             step = steps[stepIndex-1];
@@ -145,11 +215,11 @@ define([
                 case 'step2':
                     $('#step2').show();
                     console.log("---------------------------------------------------------------------------------------------------------------->This is step 2");
-                  /*  connection.trigger('updateButton', {
-                        button: 'back',
-                        visible: true
-                    });
-                        connection.trigger('updateButton', {
+                    // connection.trigger('updateButton', {
+                    //     button: 'back',
+                    //     visible: true
+                    // });
+                         connection.trigger('updateButton', {
                         button: 'next',
                         text: 'next',
                         visible: true
@@ -168,17 +238,17 @@ define([
                     visible: true
                 });
                 break;
-            }*/
+            }
             function save() {
 
-                var accountSid = $('#accountSid').val();
+            //   var accountSid = $('#accountSid').val();
                 var authToken = $('#authToken').val();
             //    var messagingService = $('#messagingService').val();
                 var body = $('#messageBody').val();
                // console.log("in the save option "+ body);
                 
                 payload['arguments'].execute.inArguments = [{
-                    "accountSid": accountSid,
+            //        "accountSid": accountSid,
                     "authToken": authToken,
             //        "messagingService": messagingService,
                     "body": body,
@@ -186,24 +256,13 @@ define([
                    
                 }];       
                 payload['metaData'].isConfigured = true;
-                console.log("Payload on SAVE function: "+JSON.stringify(payload));
+
+                console.log("Payload on SAVE function Update--------------------------------------------------->: " + JSON.stringify(payload));
+                payload1 = JSON.stringify(payload);
+                console.log("This is payload 1 in save function-------> " + payload1);
                 connection.trigger('updateActivity', payload);
         
             } 
 
 });
 
-// //function sendMessageFor (token, channel) {
-//     console.log('in the function of sendmessage for ')
-//     const baseUrl = `https://api.telegram.org/bot${token}`
-
-//     return message => {
-//     const urlParams = querystring.stringify({
-//         chat_id: channel,
-//         text: 'hello i am bot hit by journey builder',
-//         parse_mode: 'HTML'
-//     })
-
-//     return sendRequest(`${baseUrl}/sendMessage?${urlParams}`)
-//     }
-// }-->
