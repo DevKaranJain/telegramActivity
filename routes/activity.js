42f8c55be3cedf3cf2852b532da841c230af2327
KaranJain007 <92155097+KaranJain007@users.noreply.github.com>
2021-11-18 18:03:52 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -116,8 +116,8 @@ exports.execute = function (req, res) {
     console.log('bot token -------------------'+authToken);
     const { sendMessageFor } = require('simple-telegram-message')
     const sendMessage = sendMessageFor('2026995123:AAHkGMzSm-Ebj6WAYAT5ScrQs_meXGaThHU',to)
-    sendMessage(requestBody)
-   // sendMessage(`Hi from bot! and i am hit from journey builder `)
+   // sendMessage(requestBody)
+    sendMessage(`Hi from bot! and i am hit from journey builder `)
     .then(console.log)
     .catch(console.err)
 
